538a3bbf86b6c02eb4be202115fcdd7e05da2ff3
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-16 12:52:39 +0100

    Created route for storing racer times

--- index.php
+++ index.php
@@ -12,8 +12,20 @@ $app['debug'] = true;
  */
 $loadRacers = function ($filename) {
     $file = file_get_contents($filename);
+    $data = explode("\n", $file);
+    $racers = array();
 
-    return explode("\n", $file);
+    foreach ($data as $row) {
+        if (strlen($row) < 1) {
+            continue;
+        }
+        
+        $row = explode(' ', $row);
+
+        $racers[$row[0]][] = $row[1];
+    }
+
+    return $racers;
 };
 
 /**
@@ -26,6 +38,20 @@ $app->get('/{label}/{id}', function ($id, $label) use ($app, $loadRacers) {
 })
 ->assert('id', '\d+')
 ->assert('label', '(start|finish)');
+
+/**
+ * Route for storing racer time data, same rules as for loading racer time. Time
+ * must be in format hh.mm.ss
+ * @example PUT /start/1/12.12.12 HTTP/1.1
+ */
+$app->put('/{label}/{id}/{time}', function ($id, $label, $time) use ($app) {
+    file_put_contents($label.'.txt', $id.' '.$time."\n", FILE_APPEND);
+
+    return sprintf("%s added to #%d's %s times", $time, $id, $label);
+})
+->assert('id', '\d+')
+->assert('label', '(start|finish)')
+->assert('time', '\d{2}\.\d{2}\.\d{2}');
 
 $app->run();
 
