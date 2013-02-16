d1bb18ef43527ecde5a97e6a5c6269dfd96d46b8
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-16 12:40:15 +0100

    Created basic functionality to load racer times

--- index.php
+++ index.php
@@ -4,9 +4,28 @@ require __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/{label}/{id}', function ($id, $label) use ($app) {
-    return 'Racer #'.$app->escape($id).' not found';
-})->assert('id', '\d+');
+$app['debug'] = true;
+
+/**
+ * Loads racers from a textfile and returns them as an array
+ * @var string
+ */
+$loadRacers = function ($filename) {
+    $file = file_get_contents($filename);
+
+    return explode("\n", $file);
+};
+
+/**
+ * Route for loading racer time data, label must be "start" or "finish", id must
+ * be numeric. Returns result as JSON.
+ * @example GET /start/1 HTTP/1.1
+ */
+$app->get('/{label}/{id}', function ($id, $label) use ($app, $loadRacers) {
+    return json_encode($loadRacers($label.'.txt'));
+})
+->assert('id', '\d+')
+->assert('label', '(start|finish)');
 
 $app->run();
 
